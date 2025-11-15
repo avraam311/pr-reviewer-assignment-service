dd7cc64f2ab9971162465818cc368423c7f70fef
Ibragim <yandaroviba@mail.ru>
2025-11-15 17:17:23 +0300

    add config/local.yaml and .env

--- internal/repository/teams/repository.go
+++ internal/repository/teams/repository.go
@@ -21,8 +21,8 @@ type Repository struct {
 func New(cfg *config.Config) (*Repository, error) {
 	dsn := fmt.Sprintf(
 		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
-		cfg.GetString("DB_HOST"), cfg.GetInt("DB_PORT"), cfg.GetString("DB_user"),
-		cfg.GetString("DB_PASSWORD"), cfg.GetString("DB_NAME"), cfg.GetString("DB_SSL"),
+		cfg.GetString("DB_HOST"), cfg.GetInt("DB_PORT"), cfg.GetString("DB_USER"),
+		cfg.GetString("DB_PASSWORD"), cfg.GetString("DB_NAME"), cfg.GetString("DB_SSL_MODE"),
 	)
 	poolConfig, err := pgxpool.ParseConfig(dsn)
 	if err != nil {
@@ -31,7 +31,6 @@ func New(cfg *config.Config) (*Repository, error) {
 
 	poolConfig.MaxConns = cfg.GetInt32("db.max_conns")
 	poolConfig.MaxConnLifetime = cfg.GetDuration("db.max_conn_lifetime")
-	poolConfig.MaxConnIdleTime = cfg.GetDuration("db.max_conn_idletime")
 
 	pool, err := pgxpool.NewWithConfig(context.Background(), poolConfig)
 	if err != nil {
