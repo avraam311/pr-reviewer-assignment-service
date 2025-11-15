f578ad2dbca809cf25a048a1975301cf07d9c3d9
Ibragim <yandaroviba@mail.ru>
2025-11-15 13:19:28 +0300

    update: change responses.go as in openapi.yaml; todo: fix teams handler responses handling

---
+++ internal/api/http/responses/responses.go
@@ -0,0 +1,50 @@
+package responses
+
+import (
+	"net/http"
+
+	"github.com/avraam311/pr-reviewer-assignment-service/internal/infra/logger"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	ErrCodeTeamExists  = "TEAM_EXISTS"
+	ErrCodePrExists    = "PR_EXISTS"
+	ErrCodePrMerged    = "PR_MERGED"
+	ErrCodeNotAssigned = "NOT_ASSIGNED"
+	ErrCodeNoCandidate = "NO_CANDIDATE"
+	ErrCodeNotFound    = "NOT_FOUND"
+)
+
+type Success struct {
+	Result interface{} `json:"result"`
+}
+
+type ErrorResponse struct {
+	Error struct {
+		Code    string `json:"code"`
+		Message string `json:"message"`
+	} `json:"error"`
+}
+
+func ResponseOK(c *gin.Context, result interface{}) {
+	c.JSON(http.StatusOK, Success{Result: result})
+}
+
+func ResponseCreated(c *gin.Context, result interface{}) {
+	c.JSON(http.StatusCreated, Success{Result: result})
+}
+
+func ResponseError(c *gin.Context, code string, message string, statusCode int) {
+	resp := ErrorResponse{}
+	resp.Error.Code = code
+	resp.Error.Message = message
+	c.JSON(statusCode, resp)
+	c.Abort()
+}
+
+func HandleError(c *gin.Context, err error, code string, message string, statusCode int) {
+	logger.Logger.Error().Err(err).Msg(message)
+	ResponseError(c, code, message, statusCode)
+}
